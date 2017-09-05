88c825caec6dece2bfb29920d633fdc8529b9e91
maugelves <mgelves@gmail.com>
2017-09-05 15:03:12 +0200

    Added: Classes model and services for OpenJob

---
+++ inc/classes/openjob-model.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace MGJO\models;
+
+class OpenJob {
+
+	// Variables
+	private $ID                 = 0;
+	private $title              = 0;
+	private $place              = null;
+	private $responsabilities   = null;
+	private $requirements       = null;
+	private $salarymin          = null;
+	private $salarymax          = null;
+
+
+	// GETTERS AND SETTERS
+	/**
+	 * @return int
+	 */
+	public function get_ID() {
+		return $this->ID;
+	}
+
+	/**
+	 * @param int $ID
+	 */
+	public function set_ID( $ID ) {
+		$this->ID = $ID;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function get_title() {
+		return $this->title;
+	}
+
+	/**
+	 * @param int $title
+	 */
+	public function set_title( $title ) {
+		$this->title = $title;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function get_place() {
+		return $this->place;
+	}
+
+	/**
+	 * @param null $place
+	 */
+	public function set_place( $place ) {
+		$this->place = $place;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function get_responsabilities() {
+		return $this->responsabilities;
+	}
+
+	/**
+	 * @param null $responsabilities
+	 */
+	public function set_responsabilities( $responsabilities ) {
+		$this->responsabilities = $responsabilities;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function get_requirements() {
+		return $this->requirements;
+	}
+
+	/**
+	 * @param null $requirements
+	 */
+	public function set_requirements( $requirements ) {
+		$this->requirements = $requirements;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function get_salarymin() {
+		return $this->salarymin;
+	}
+
+	/**
+	 * @param null $salarymin
+	 */
+	public function set_salarymin( $salarymin ) {
+		$this->salarymin = $salarymin;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function get_salarymax() {
+		return $this->salarymax;
+	}
+
+	/**
+	 * @param null $salarymax
+	 */
+	public function set_salarymax( $salarymax ) {
+		$this->salarymax = $salarymax;
+	}
+
+}
---
+++ inc/classes/openjobs-service.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace MGJO\Service;
+
+use MGJO\models\OpenJob;
+use MGJO\Helpers;
+
+class OpenJobs extends Helpers\Singleton {
+
+	/**
+	 * Create a OpenJob entity from a WP_Post instance
+	 *
+	 * @author  Mauricio Gelves <mg@maugeles.com>
+	 * @param   \WP_Post $post
+	 * @param   $fill_fields    bool    if true the function fills all the object fields
+	 * @return  OpenJob
+	 * @since   1.0
+	 */
+	public function get_openjob_from_post( \WP_Post $post, $fill_fields = true ) {
+
+		// Variable
+		$openjob       = new OpenJob();
+
+		// Set Values
+		$openjob->set_ID( $post->ID );
+		$openjob->set_title( $post->post_title );
+
+		// Should the function complete the rest of the fields
+		if( $fill_fields ):
+
+			$openjob_meta  = get_fields( $post->ID );
+
+			if( !empty( $openjob_meta['mgjo-place'] ) ) $openjob->set_place( esc_html( $openjob_meta['mgjo-place'] ) );
+			if( !empty( $openjob_meta['mgjo-responsibility'] ) ) $openjob->set_responsabilities( esc_html( $openjob_meta['mgjo-responsibility'] ) );
+			if( !empty( $openjob_meta['mgjo-requirements'] ) ) $openjob->set_requirements( esc_html( $openjob_meta['mgjo-requirements'] ) );
+			if( !empty( $openjob_meta['mgjo-salary-min'] ) ) $openjob->set_salarymin( esc_html( $openjob_meta['mgjo-salary-min'] ) );
+			if( !empty( $openjob_meta['mgjo-salary-max'] ) ) $openjob->set_salarymin( esc_html( $openjob_meta['mgjo-salary-max'] ) );
+
+
+		endif;
+
+		return $openjob;
+
+	}
+
+
+
+	/**
+	 * Return all the available OpenJobs
+	 *
+	 * @author  Mauricio Gelves <mg@maugelves.com>
+	 * @param   $args   array
+	 * @return  array   Array of OpenJob Objects
+	 */
+	public function get_all( $args = null ) {
+
+		// Variables
+		$openjobs = array();
+
+		// Parse Args
+		$args = wp_parse_args( $args, array(
+			'posts_per_page' => 10,
+			'posts_per_archive_page' => 10
+		));
+
+		// Add basic filter
+		$args['post_type']      = 'joboffer';
+		$args['post_status']    = 'publish';
+
+		// Execute the Query
+		$query = new \WP_Query( $args );
+
+		if( $query->have_posts() ):
+
+			while( $query->have_posts() ): $query->the_post();
+
+				$openjob = $this->get_openjob_from_post( $query->post );
+				array_push( $openjobs, $openjob );
+
+			endwhile;
+
+		endif;
+
+		return $openjobs;
+
+	}
+
+}
