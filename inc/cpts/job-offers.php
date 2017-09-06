0e468d1b2e00622decb8d6c84e2cf9191299c6f9
maugelves <mgelves@gmail.com>
2017-09-06 12:26:16 +0200

    Added: Editor feature to the JobOffer Custom Post Type

--- inc/cpts/job-offers.php
+++ inc/cpts/job-offers.php
@@ -110,7 +110,7 @@ class JobOffers
 			'show_ui'               => true,
 			'menu_position'         => 19,
 			'menu_icon'             => MGJO_URL . '/assets/img/joboffer-icon.png',
-			'supports'              => array( 'title' ),
+			'supports'              => array( 'title', 'editor' ),
 			'has_archive'           => true,
 			'capabilities'          => $capabilities,
 			'mat_meta_cap'          => true,
