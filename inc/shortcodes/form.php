b3e874b0faf6a7442cc5a272d6c1bb676b41a453
maugelves <mgelves@gmail.com>
2017-09-06 15:54:42 +0200

    Added: new fields to the shortcode wichs render the form

--- inc/shortcodes/form.php
+++ inc/shortcodes/form.php
@@ -23,48 +23,69 @@ function fn_mgjo_applicant_form() {
 			<input type="text" id="txtname" name="txtname">
 		</div>
 
-		<div class="mgjoform__row">
-			<label for="txtadress"><?php _e("Dirección","mg-wp-job-offers"); ?>:</label>
-			<input type="text" id="txtname" name="txtname" value="<?php echo $name; ?>">
-		</div>
+        <div class="mgjoform__row">
+            <label for="txtemail"><?php _e("Correo electrónico","mg-wp-job-offers"); ?>:</label>
+            <input type="text" id="txtemail" name="txtemail">
+        </div>
 
-		<div class="mgjoform__row">
-			<label for="txtyearsexp"><?php _e("Años de experiencia","mg-wp-job-offers"); ?>:</label>
-			<input type="text" id="txtyearsexp" name="txtyearsexp" value="<?php echo $yearsexp; ?>">
-		</div>
+        <div class="mgjoform__row">
+            <label for="txtmobile"><?php _e("Teléfono móvil","mg-wp-job-offers"); ?>:</label>
+            <input type="text" id="txtmobile" name="txtmobile">
+        </div>
 
-		<div class="mgjoform__row">
-			<label for="txtexpdescription"><?php _e("Descripción de la experiencia","mg-wp-job-offers"); ?>:</label>
-			<textarea id="txtexpdescription" name="txtexpdescription"><?php echo $expdescripciont; ?></textarea>
-		</div>
+        <div class="mgjoform__row">
+            <label for="txtcity"><?php _e("Ciudad","mg-wp-job-offers"); ?>:</label>
+            <input type="text" id="txtcity" name="txtcity">
+        </div>
 
-		<div class="mgjoform__row">
-			<label for="txtcv"><?php _e("CV","mg-wp-job-offers"); ?>:</label>
-			<input type="file" id="txtcv" name="txtcv">
-		</div>
+        <div class="mgjoform__row">
+            <label for="txtcountry"><?php _e("País de residencia","mg-wp-job-offers"); ?>:</label>
+            <input type="text" id="txtcountry" name="txtcountry">
+        </div>
 
-		<?php
-		// Todo: Si existen valores en la variable post, hacer un bucle para crear el formulario y pintar los valoes
-		?>
-		<div class="mgjoform__row">
-			<label for="txtidioma_1"><?php _e("Idioma","mg-wp-job-offers"); ?>:</label>
-			<input type="text" id="txtidioma_2" name="txtidioma_2">
-			<input type="text" id="txtidioma_1" name="txtidioma_1">
-		</div>
+        <div class="mgjoform__row">
+            <label for="txtnationality"><?php _e("Nacionalidad","mg-wp-job-offers"); ?>:</label>
+            <input type="text" id="txtnationality" name="txtnationality">
+        </div>
 
-		<?php
-		// Todo: Si existen valores en la variable post, hacer un bucle para crear el formulario y pintar los valoes
-		?>
-		<div class="mgjoform__row">
-			<label for="txtit"><?php _e("Conocimientos informáticos","mg-wp-job-offers"); ?>:</label>
-			<textarea id="txtit" name="txtit"></textarea>
-		</div>
+        <div class="mgjoform__row">
+            <label for="txtbirthdate"><?php _e("Fecha de nacimiento","mg-wp-job-offers"); ?>:</label>
+            <input type="text" id="txtbirthdate" name="txtbirthdate" placeholder="dd/mm/aaaa">
+        </div>
 
-		<div class="mgjoform__row">
-			<label for="txtdegree_1"><?php _e("Educación","mg-wp-job-offers"); ?>:</label>
-			<input type="text" id="txtdegree_1" name="txtdegree_1">
-			<input type="text" id="txtdegree_2" name="txtdegree_2">
-		</div>
+        <div class="mgjoform__row">
+            <label for="ddlgender"><?php _e("Sexo","mg-wp-job-offers"); ?>:</label>
+            <select name="ddlgender" id="ddlgender">
+                <option value="f"><?php _e('Femenino', 'mg-wp-job-offers'); ?></option>
+                <option value="m"><?php _e('Masculino', 'mg-wp-job-offers'); ?></option>
+            </select>
+        </div>
+
+        <div class="mgjoform__row">
+            <label for="ddlmovility"><?php _e("Movilidad geográfica","mg-wp-job-offers"); ?>:</label>
+            <select name="ddlmovility" id="ddlmovility">
+                <option value="1">Sí</option>
+                <option value="0">No</option>
+            </select>
+        </div>
+
+
+        <div class="mgjoform__row">
+            <label for="ddlmovility"><?php _e("Años de experiencia","mg-wp-job-offers"); ?>:</label>
+            <?php
+                $args = array(
+                    'name'          => 'ddlmovility',
+                    'hide_empty'    => 0,
+                    'id'            => 'ddlmovility',
+                    'taxonomy'      => 'jobexperience'
+                );
+
+                wp_dropdown_categories( $args );
+
+            ?>
+        </div>
+
+        <?php //TODO: Agregar el ID de la oferta de trabajo en un hidden ?>
 
 		<input type="hidden" name="action" value="newapplicant">
 
